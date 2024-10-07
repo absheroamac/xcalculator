efa59cca2230e3d6170a885ee4081ef46cd059fa
Absher K <absheroamac@gmail.com>
2024-10-07 20:18:00 +0530

    Completed Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import styles from './App.module.css'
 
 function App() {
+
+  const [expression,setExpression] = useState("");
+  const [answer,setAnswer] = useState("")
+
+  const handleCalculation = ()=>{
+    if(expression===""){
+      setAnswer("Error")
+    }
+    else{
+      setAnswer(eval(expression))
+    }
+  }
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className={styles.app}>
+      <h1>React Calculator</h1>
+      <input value={expression} className="input-field">
+      </input>
+      <div>{answer}</div>
+
+      <div className={styles.buttonContainer}>
+      <button className={styles.button} onClick={()=>setExpression(expression+"7")}>7</button>
+      <button className={styles.button} onClick={()=>setExpression(expression+"8")}>8</button>
+      <button className={styles.button} onClick={()=>setExpression(expression+"9")}>9</button>
+      <button className={styles.button} onClick={()=>setExpression(expression+"+")}>+</button>
+      <button className={styles.button} onClick={()=>setExpression(expression+"4")}>4</button>
+      <button className={styles.button} onClick={()=>setExpression(expression+"5")}>5</button>
+      <button className={styles.button} onClick={()=>setExpression(expression+"6")}>6</button>
+      <button className={styles.button} onClick={()=>setExpression(expression+"-")}>-</button>
+      <button className={styles.button} onClick={()=>setExpression(expression+"1")}>1</button>
+      <button className={styles.button} onClick={()=>setExpression(expression+"2")}>2</button>
+      <button className={styles.button} onClick={()=>setExpression(expression+"3")}>3</button>
+      <button className={styles.button} onClick={()=>setExpression(expression+"*")}>*</button>
+      <button className={styles.button} onClick={()=>{setExpression(""); setAnswer("")}}>C</button>
+      <button className={styles.button} onClick={()=>setExpression(expression+"0")}>0</button>
+      <button className={styles.button} onClick={handleCalculation}>=</button>
+      <button className={styles.button} onClick={()=>setExpression(expression+"/")}>/</button>
+      </div>
     </div>
   );
 }
